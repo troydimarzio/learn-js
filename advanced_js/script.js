32a4813964eb8072bf4fb32ea639dcb78a602683
troydimarzio <troymokoagow86@gmail.com>
2020-11-10 00:10:18 +0800

    add folder advanced-js

---
+++ advanced_js/script.js
@@ -0,0 +1,159 @@
+/* prototype Object
+
+* Object Literal
+* Object function declaration
+* Object constructor (new keyword)
+* object.create()
+
+*/
+
+/* ======================== Object Lietal ==============================
+
+* object punya property dan method
+* property : value
+* method : function
+* di dalam object ada keyword this yg mana mengacu pada object tersebut.
+* string literal > standar ES6
+
+====================================================================== */
+
+let mahasiswa = {
+    nama: "troy",
+    umur: 22,
+    menyapa: function(teman){
+        return `halo, ${this.nama}. Umur kamu ${this.umur} ya? Kenalan yuk dengan ${teman}!`;
+    }
+};
+
+/* ======================== Object function ==============================
+
+* hanya membuat satu kali blueprint / template dan dapat dipanggil beruang kali
+* menggunakan deklarasi object dan return objectnya di dalam function
+
+====================================================================== */
+
+// function Keluarga(nama, marga){
+//     let keluarga = {};
+//     keluarga.nama = nama;
+//     keluarga.marga = marga;
+//     keluarga.fullname = function(){
+//         return `halo ${this.nama} ${this.marga}`;
+//     }
+
+//     return keluarga;
+// }
+
+// let troy = Keluarga('troy', 'mokoagow');
+// let alvin = Keluarga('alvin', 'mokoagow');
+
+/* ======================== Object constructor ==============================
+
+* menggunakan keyword new
+* tidak perlu lagi mendeklarasikan object dan menggunakan return
+* ganti dengan this
+
+====================================================================== */
+
+// function Keluarga(nama, marga){
+//     this.nama = nama;
+//     this.marga = marga;
+//     this.fullname = function(){
+//         return `halo ${this.nama} ${this.marga}`;
+//     }
+// }
+
+// let drifan = new Keluarga("drifan", "mokoagow");
+
+/* ======================== Object.create() ==============================
+
+* sama seperti mendeklarasikan objek
+* hanya saja objek.create() ada parapeternya
+* sama seperti konsep inheritance
+
+====================================================================== */
+
+const keluargaParent = {
+    fullname: function(){
+        return `halo ${this.nama} ${this.marga}`;
+    }
+};
+
+function Keluarga(nama, marga){
+    let keluarga = Object.create(keluargaParent);
+    keluarga.nama = nama;
+    keluarga.marga = marga;
+
+    return keluarga;
+}
+
+let kel = Keluarga("Nyasia", "Langworth");
+
+/* ======================== Konsep prototype ==============================
+
+* prototype secara tidak langsung dibuatkan oleh javascript
+* setiap array, objek, number mapupun class2 yg lain punya prototype
+
+====================================================================== */
+
+function Keluarga(nama, marga){
+    // let this = Object.create(this.prototype);
+    this.nama = nama;
+    this.marga = marga;
+
+    // return this;
+}
+
+Keluarga.prototype.fullname = function(){
+    return `halo ${this.nama}`;
+}
+
+let kel1 = new Keluarga("Johnathon", "Bednar");
+
+/* ======================== Arrow Function ==============================
+
+* function expression yg lebih singkat
+
+====================================================================== */
+
+const fullName = (firstName, lastName) => {
+    return `halo ${firstName} ${lastName}`;
+}
+
+/* ======================== High Order Function -> reduce, filter, map ==============================
+
+* high order function -> function yg di dalamnya ada function lain sebagai parameter atau return function
+                      -> function sebagai argumen/parameter disebut callback -> anonymous function
+                      -> function yg memiliki callback disebut high order function
+* filter -> memfilter elemen di dalam array
+* map -> memetakan elemen array ke dalam array baru
+* reduce -> menjumlahkan semua elemen array
+
+====================================================================== */
+// for biasa
+let angka = [4, 0, 10, 2, 3];
+let arr = [];
+for(let i = 0; i < angka.length; i++){
+    if(angka[i] >= 3){
+        arr.push(angka[i]);
+    }
+}
+
+//filter
+const newAngka1 = angka.filter(function(curr){
+    return curr >= 3;
+});
+// map
+const map = angka.map(function(curr){
+    return curr * 2;
+});
+// reduce
+const reduce = angka.reduce(function(prev, curr){
+    return prev + curr;
+});
+// method chaining -> menyederhanakan penulisan method prototype HOF
+const newAngka = angka.filter(curr => curr > 2)
+    .map(curr => curr * 2)
+    .reduce((prev, curr) => prev + curr);
+    
+
+console.log(newAngka);
